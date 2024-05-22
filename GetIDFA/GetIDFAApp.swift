d0b6e8fd2f2c11ccba3dc619ffe81da10f5144a1
Caner Ates <cnrates@gmail.com>
2024-05-22 12:02:16 +0300

    Initial Commit

---
+++ GetIDFA/GetIDFAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetIDFAApp.swift
+//  GetIDFA
+//
+//  Created by Caner Ates on 22.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GetIDFAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
